adbc90e482f6a6a47fcb4102f71d64243ddea6a3
Bob Murmu <bobmurmu02@gmail.com>
2024-01-13 18:40:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import data from './data.json';
+
 
 function App() {
+  // const [currencies, setCurrencies] = useState({});
+  const [currencyOne, setCurrencyOne] = useState('INR');
+  const [currencyTwo, setCurrencyTwo] = useState('USD');
+  const [conversionResult, setConversionResult] = useState('');
+
+  // useEffect(()=>{
+  //   async function fetchExchangeRates(){
+
+  //     try{
+
+  //       const response = await fetch("https://api.freecurrencyapi.com/v1/latest",{
+  //         method: 'GET',
+  //         mode: 'cors',
+  //         headers: {
+  //           apikey: 'fca_live_iqiqFpAQQ22mIlCxqTG5rCtY212ORdLtKkQMPnqo'
+  //         }
+  //       });
+
+  //       if(response.ok){
+  //         const data = await response.json();
+  //         console.log(data.data)
+  //         setCurrencies(data.data);
+  //       }else{
+  //         console.error("Error fetching currency data");
+  //       }
+
+  //     }catch(error){
+
+  //       console.error("Error fetching resources : ", error);
+
+  //     }
+  //   }
+  //   fetchExchangeRates();
+  // },[])
+
+  const convertCurrency = () =>{
+    const answer = data[currencyTwo]/data[currencyOne];
+
+    setConversionResult(answer);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div 
+      className="App w-1/2 h-1/2 flex flex-col justify-between items-center bg-white rounded-3xl">
+
+      <h1 className='title text-center text-3xl'>Currency Converter</h1>
+
+      <div className="inputContainer flex">
+        <form action="#">
+          <label htmlFor="currency-one">
+            <select 
+              name="currency" id="currency-one" value={currencyOne} onChange={(e)=>setCurrencyOne(e.target.value)}
+              className='w-40'>
+              {
+                Object.keys(data).map((currency)=>(
+                <option key={currency} value={currency}>
+                  {currency}
+                </option>
+              ))}
+            </select>
+          </label>
+        </form>
+
+        <form action="#">
+          <label htmlFor="currency-two">
+            <select 
+              name="currency" id="currency-two" value={currencyTwo} onChange={(e)=>setCurrencyTwo(e.target.value)}
+              className='w-40'>
+              {Object.keys(data).map((currency, index)=>(
+                <option key={index} value={currency}>
+                  {currency}
+                </option>
+              ))}
+            </select>
+          </label>
+        </form>
+      </div>
+
+      <input onClick={convertCurrency} type="submit" id='submit'/>
+
+      <h2 className='answer'>{conversionResult}</h2>
+
     </div>
   );
+
 }
 
 export default App;
